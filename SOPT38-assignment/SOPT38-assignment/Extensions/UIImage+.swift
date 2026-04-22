f87444a5301cad79c5151ef51cf3d1e6a0416b29
Seoyoung Lee <lsyamyseoyoung3040@gmail.com>
2026-04-23 03:40:13 +0900

    feat: add icons

---
+++ SOPT38-assignment/SOPT38-assignment/Extensions/UIImage+.swift
@@ -0,0 +1,20 @@
+//
+//  UIImage+.swift
+//  SOPT38-assignment
+//
+//  Created by Seoyoung Lee on 4/23/26.
+//
+
+import UIKit
+
+extension UIImage {
+    enum Watcha {
+        static let checkOff = UIImage(named: "check-off")
+        static let checkOn = UIImage(named: "check-on")
+        static let closeSquare = UIImage(named: "Close Square")
+        static let enableOff = UIImage(named: "enable-off")
+        static let enableOn = UIImage(named: "enable-on")
+        static let eyeOff = UIImage(named: "eye-off")
+        static let eyeOn = UIImage(named: "eye-on")
+    }
+}
