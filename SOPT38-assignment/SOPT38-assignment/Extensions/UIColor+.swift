da27ccfeaf6a023d2a0bec1ae505e8c397da8a38
Seoyoung Lee <lsyamyseoyoung3040@gmail.com>
2026-04-23 01:09:04 +0900

    feat: add design system

---
+++ SOPT38-assignment/SOPT38-assignment/Extensions/UIColor+.swift
@@ -0,0 +1,23 @@
+//
+//  UIColor+.swift
+//  SOPT38-assignment
+//
+//  Created by Seoyoung Lee on 4/23/26.
+//
+
+import UIKit
+
+extension UIColor {
+    enum Watcha {
+        static let gray100 = UIColor(named: "gray100")!
+        static let gray200 = UIColor(named: "gray200")!
+        static let gray300 = UIColor(named: "gray300")!
+        static let gray400 = UIColor(named: "gray400")!
+        static let gray500 = UIColor(named: "gray500")!
+        static let gray600 = UIColor(named: "gray600")!
+        static let white = UIColor(named: "white")!
+        static let black = UIColor(named: "black")!
+        static let pink = UIColor(named: "pink")!
+        static let green = UIColor(named: "green")!
+    }
+}
