3c6fd3f1adf5ba65909382760ff95179c1596747
Seoyoung Lee <lsyamyseoyoung3040@gmail.com>
2026-05-01 16:22:59 +0900

    chore: 디렉토리 정리

--- SOPT38-assignment/SOPT38-assignment/Application/SceneDelegate.swift
+++ SOPT38-assignment/SOPT38-assignment/Application/SceneDelegate.swift
@@ -15,12 +15,9 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
             
-        let loginViewController = LoginViewController()
-        let navigationController = UINavigationController(rootViewController: loginViewController)
-            
         let window = UIWindow(windowScene: windowScene)
-        window.rootViewController = navigationController
-        
+        window.backgroundColor = .black
+        window.rootViewController = TabBarController()
         window.makeKeyAndVisible()
         self.window = window
     }
