775a3d658d63ca341ef90a7119d8eeaab52e2216
Seoyoung Lee <lsyamyseoyoung3040@gmail.com>
2026-05-01 20:41:57 +0900

    feat: release section

---
+++ SOPT38-assignment/SOPT38-assignment/Presentation/ReleaseContentCollectionViewCell.swift
@@ -0,0 +1,38 @@
+//
+//  ReleaseContentCollectionViewCell.swift
+//  SOPT38-assignment
+//
+//  Created by Seoyoung Lee on 5/1/26.
+//
+
+import UIKit
+
+import Then
+import SnapKit
+
+class ReleaseCollectionViewCell: UICollectionViewCell {
+    
+    static let identifier = "ReleaseCollectionViewCell"
+
+    override init(frame: CGRect) {
+        super.init(frame: .zero)
+        contentView.addSubview(posterImageView)
+        posterImageView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private let posterImageView = UIImageView().then {
+        $0.image = .miniPoster1
+    }
+}
+
+extension ReleaseCollectionViewCell {
+    func dataBind(_ releaseData: ReleaseModel) {
+        posterImageView.image = releaseData.posterImg
+    }
+}
