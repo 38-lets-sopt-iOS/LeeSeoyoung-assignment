a184d24532d382f1f2cf44b24d3b7b12771fa816
Seoyoung Lee <lsyamyseoyoung3040@gmail.com>
2026-05-29 03:37:25 +0900

    chore: SwiftUI 프로젝트 추가

---
+++ SOPT38-SwiftUI-assignment/SOPT38-SwiftUI-assignment/SOPT38_SwiftUI_assignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  SOPT38_SwiftUI_assignmentApp.swift
+//  SOPT38-SwiftUI-assignment
+//
+//  Created by Seoyoung Lee on 5/29/26.
+//
+
+import SwiftUI
+
+@main
+struct SOPT38_SwiftUI_assignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
